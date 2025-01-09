46ebc2e920c458bf066d81e93fc85153a962739d
KalybaiNurdaulet <nurdauietzhhhh@gmail.com>
2025-01-09 15:34:02 +0500

    initial commit

--- src/syntax/Main.java
+++ src/syntax/Main.java
@@ -1,10 +1,10 @@
+package syntax;
 
-//this Main class
+//this syntax.Main class
 public class Main {
     // this method main entry point to our programm
     public static void main(String[] args) {
 
         System.out.println("Nurik");
-//changing 1
     }
 }
